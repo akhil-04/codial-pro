93ac5cf93a0153260a9ac37c4b149817af88ca2d
akhil-04 <svam1008@gmail.com>
2020-09-12 10:57:45 +0530

    first commit: create the directory and setup our express server

---
+++ index.js
@@ -0,0 +1,17 @@
+// require the express
+const express =  require('express');
+
+//run that express
+const app = express();
+
+//port on which we run our server
+const port = 8000;
+
+//bind connection with host and server
+app.listen(port, function(err){
+    if(err){
+        //interpolation is here
+        console.log(`error in running server: ${err}`);
+    }
+    console.log(`Server is running on port: ${port}`);
+});
