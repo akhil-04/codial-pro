f2c0e7b3d42baa3df38fe0c70898cc354f77bc26
akhil-04 <svam1008@gmail.com>
2020-09-17 20:31:19 +0530

    added the sign in and up form and cookie explanation

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,3 +7,25 @@ module.exports.profile = function(req, res){
 module.exports.post = function(req, res){
    return res.end('<h1>This is the post of users </h1>');
 };
+//render the sign up page
+module.exports.signUp = function(req, res){
+   return res.render('user_sign_up',{
+      title: "Codeial | sign Up"
+   });
+}
+//render the sign in page
+module.exports.signIn = function(req, res){
+   return res.render('user_sign_in',{
+      title: "Codeial | sign In"
+   });
+}
+
+//get the sign up data here
+module.exports.create = function(){
+   //todo later
+}
+
+//cr
+module.exports.createSession = function(){
+   //todo later
+}
